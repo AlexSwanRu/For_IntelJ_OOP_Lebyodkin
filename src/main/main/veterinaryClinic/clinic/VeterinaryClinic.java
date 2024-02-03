4a9982678477c543a8b4eee9c8b3e127bb179e71
Alex <lav1ru@mail.ru>
2024-02-03 20:34:12 +0300

    Last Edition1 to HomeTask02

---
+++ src/main/main/veterinaryClinic/clinic/VeterinaryClinic.java
@@ -0,0 +1,36 @@
+package main.veterinaryClinic.clinic;
+
+import main.clients.pets.Animal;
+import main.veterinaryClinic.colleague.Workerl;
+
+import java.util.ArrayList;
+import java.util.List;
+public class VeterinaryClinic {
+   protected List<Workerl> worker;
+   protected List<Animal>patiens;
+
+   public void addEmployee(Workerl.employee) {
+      worker.add(employee);
+   }
+   public void addPatient(Animal.patient) {
+      patiens.add((Animal.patient);
+   }
+      worker.add(w);
+
+
+   public List<Animal> getPatiens() {
+      return patiens;
+         }
+
+   public List<Workerl> getWorker() {
+      return worker;
+   }
+
+   public VeterinaryClinic() {
+      this.worker = new ArrayList<>();
+      this.patiens=new ArrayList<>();
+
+      }
+}
+
+
